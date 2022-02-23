93a887051c933f6224d02b61c461be19d80a2cd1
Patryk Zawadzki <patrys@room-303.com>
2022-02-23 13:41:17 +0100

    First draft of an alternative metadata API

--- saleor/graphql/channel/types.py
+++ saleor/graphql/channel/types.py
@@ -77,9 +77,19 @@ class ChannelContextTypeWithMetadataForObjectType(ChannelContextTypeForObjectTyp
         return ObjectWithMetadata.resolve_metadata(root.node, info)
 
     @staticmethod
+    def resolve_metafields(root: ChannelContext, info, keys=None):
+        # Used in metadata API to resolve metadata fields from an instance.
+        return ObjectWithMetadata.resolve_metafields(root.node, info, keys=keys)
+
+    @staticmethod
     def resolve_private_metadata(root: ChannelContext, info):
         # Used in metadata API to resolve private metadata fields from an instance.
         return ObjectWithMetadata.resolve_private_metadata(root.node, info)
+
+    @staticmethod
+    def resolve_private_metafields(root: ChannelContext, info, keys=None):
+        # Used in metadata API to resolve private metadata fields from an instance.
+        return ObjectWithMetadata.resolve_private_metafields(root.node, info, keys=keys)
 
 
 class ChannelContextTypeWithMetadata(
